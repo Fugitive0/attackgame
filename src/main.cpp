ee2b8189a74da265f7b155402cfb47ce6583f42c
David <davidjoy.school@gmail.com>
2022-12-02 17:52:06 -0800

    Added variables
    Added variables for the overall difficulty of the game, overall exp multilplier, and overall money multiplier

---
+++ src/main.cpp
@@ -0,0 +1,96 @@
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <globalVar.h>
+
+using namespace std;
+
+
+
+/*
+The difficutly function will have an interger parameter that takes what difficulty
+the game will be. 1 being the easiest and 5 being the hardest.
+
+each increment will increase the stats by the percentage down below
+
+-----------------------------------------------------------------------
+
+Things that the difficutly will negativly affect:
+1. Health of the enemy will increase by 5%
+2. Damage of the enemy will increase by 5%
+
+Things that the difficulty will positivitly affect
+1. The amount of experince you will get will increase by 25%
+2. Money you will get will increase by 5%
+*/
+
+int gameDifficulty(int difficulty, )
+{
+  if (difficulty == 1)
+  {
+    int OVERALL_DIFF;
+    int EXP_PERCENTAGE += 25;
+    int MONEY_PERCENTAGE = 15;  
+  }
+  
+
+  if (difficulty == 2)
+  {
+    int OVERALL_DIFF;
+    int EXP_PERCENTAGE = 25;
+    int MONEY_PERCENTAGE = 15;  
+  }
+
+  if (difficulty == 3)
+  {
+    int OVERALL_DIFF;
+    int EXP_PERCENTAGE = 25;
+    int MONEY_PERCENTAGE = 15;  
+  }
+
+
+  if (difficulty == 4)
+  {
+    int OVERALL_DIFF;
+    int EXP_PERCENTAGE = 25;
+    int MONEY_PERCENTAGE = 15;  
+  }
+
+
+  if (difficulty == 5)
+  {
+    int OVERALL_DIFF;
+    int EXP_PERCENTAGE = 25;
+    int MONEY_PERCENTAGE = 15;  
+  }
+}
+
+
+
+
+
+int main ()
+{
+  int difficultyInput = 0;
+  cout << "welcome to Attack! A game that takes wits and skill!"
+       << endl
+       << "Select a difficulty between 1 and 5"
+       << endl;
+  cin >> difficultyInput;
+
+  gameDifficulty(difficultyInput);
+
+
+
+
+
+
+
+
+
+
+
+
+  // Game loop
+
+}
