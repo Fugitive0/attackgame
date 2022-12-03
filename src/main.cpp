92d52719747bd5087e37b267e9fd3f7018b4c6f3
David <davidjoy.school@gmail.com>
2022-12-02 18:32:53 -0800

    .

--- src/main.cpp
+++ src/main.cpp
@@ -16,17 +16,48 @@ each increment will increase the stats by the percentage down below
 -----------------------------------------------------------------------
 
 Things that the difficutly will negativly affect:
-1. Health of the enemy will increase by 5%
-2. Damage of the enemy will increase by 5%
+damage will increase by 5%/10%/15%/20%/25%
 
 Things that the difficulty will positivitly affect
-1. The amount of experince you will get will increase by 25%
-2. Money you will get will increase by 5%
+1. The amount of experince you will get will increase by 25%/50%/75%/100%/125%
+2. Money you will get will increase by 25%/50%/75%/100%/125%
 */
 
 int gameDifficulty(int difficulty)
 {
-
+  if (difficulty = 1)
+  {
+    OVERALL_ENEMY_DAMAGE += 3;
+    EXP_PERCENTAGE += 6;
+    MONEY_PERCENTAGE += 6;
+  }
+  if (difficulty = 2)
+  {
+    OVERALL_ENEMY_DAMAGE += 6;
+    EXP_PERCENTAGE += 12;
+    MONEY_PERCENTAGE += 12;
+  }
+  if (difficulty = 3)
+  {
+    OVERALL_ENEMY_DAMAGE += 9;
+    EXP_PERCENTAGE += 18;
+    MONEY_PERCENTAGE += 18;
+  }
+  if (difficulty = 4)
+  {
+    OVERALL_ENEMY_DAMAGE += 12;
+    EXP_PERCENTAGE += 24;
+    MONEY_PERCENTAGE += 24;
+  }
+  if (difficulty = 5)
+  {
+    OVERALL_ENEMY_DAMAGE += 15;
+    EXP_PERCENTAGE += 30;
+    MONEY_PERCENTAGE += 30;
+  }
+  
+  
+  
 }
 
 
@@ -42,6 +73,7 @@ int main ()
   cin >> difficultyInput;
 
   gameDifficulty(difficultyInput);
+  
 
 
 
